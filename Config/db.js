d826dc2801ce5279af06728228ff9820109ee9ee
Rohail Shah <rohails786@gmail.com>
2022-04-01 15:36:02 -0400

    upload ice 11 to heroku live site

---
+++ Config/db.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SessionSecret = exports.HostName = exports.LocalURI = void 0;
+exports.LocalURI = "mongodb://127.0.0.1/contacts";
+exports.HostName = "localhost";
+exports.SessionSecret = "WEBD6201SessionSecret";
+//# sourceMappingURL=db.js.map
